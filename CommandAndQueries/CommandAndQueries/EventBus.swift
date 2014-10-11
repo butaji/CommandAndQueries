6e10a69a2b9a4b7f1f58d4e1261acefd088b422e
butaji <vitaly.baum@gmail.com>
2014-10-11 22:52:12 +0400

    added async magic

--- CommandAndQueries/CommandAndQueries/EventBus.swift
+++ CommandAndQueries/CommandAndQueries/EventBus.swift
@@ -19,7 +19,11 @@ struct EventBus {
         let handlers = subscribtions.filter({ x in x.name == y})
         
         for h in handlers {
-            h.value(e)
+            
+            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+                h.value(e)
+            }
+            
         }
     }
     
